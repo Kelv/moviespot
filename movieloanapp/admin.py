a7c60bef66c29397a3f2580e482fef45a11cc7f8
Kelvin Rodriguez <kelv.rod@gmail.com>
2019-01-26 09:31:46 -0400

    Created Models

--- movieloanapp/admin.py
+++ movieloanapp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
+from movieloanapp.models import Movie
+
+admin.register(Movie)
--- movieloanapp/models.py
+++ movieloanapp/models.py
@@ -1,5 +1,3 @@
-import datetime
-
 from django.db import models
 
 
@@ -11,6 +9,7 @@ class Movie(models.Model):
 	year = models.IntegerField(default=0)
 	country = models.TextField(default='')
 	in_stock = models.IntegerField()
+	borrow_price = models.FloatField()
 
 
 class Customer(models.Model):
@@ -19,10 +18,11 @@ class Customer(models.Model):
 	address = models.TextField(blank=False)
 	phone = models.TextField(blank=False)
 	gender = models.TextField(max_length=1)
+	email = models.EmailField()
 
 
-class MovieLoan(models.Model):
+class BorrowOrder(models.Model):
 	movie = models.ForeignKey(Movie, related_name='loans')
 	customer = models.ForeignKey(Customer, related_name='customers')
-	created_date = models.DateField(auto_now=True)
+	created_date = models.DateField(auto_now=True, editable=False)
 	due_date = models.DateTimeField(blank=False)
